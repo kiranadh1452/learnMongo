43fc598fbce290161ce47ec5347b685ced6d4c6c
Kiran Adhikari <kiranadh1452@gmail.com>
2022-08-10 00:38:31 +0545

    Creating a collection and adding a document into the collection

--- mongodb-js/dbConnection.js
+++ mongodb-js/dbConnection.js
@@ -1,8 +1,33 @@
 const client = require("mongodb").MongoClient;
 const url = "mongodb://localhost:27017/myDb";
 
+const singleUserData = {
+    name: "John Rock",
+    age: 42,
+    address: "Random address over here",
+    contact: "Random contact number",
+    email: "random@random.com",
+};
+
 client.connect(url, function (err, db) {
     if (err) throw err;
     console.log("Database created");
-    db.close();
+
+    // creating a collection
+    let databaseObject = db.db("myDb");
+    databaseObject.createCollection("users", function (err, res) {
+        if (err) throw err;
+        console.log("Collection has been create successfully");
+    });
+
+    // inserting into a collection
+    databaseObject
+        .collection("users")
+        .insertOne(singleUserData, function (err, res) {
+            if (err) throw err;
+            console.log("One document has successfully been added");
+            db.close();
+        });
+
+    // close the database connection
 });
